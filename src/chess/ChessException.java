5ff8d79a9044cf0e66fdcf2da08fbc10143d02b3
Letícia Israel <106632252+leticiaffi@users.noreply.github.com>
2022-08-21 19:54:17 -0300

    Add files via upload

---
+++ src/chess/ChessException.java
@@ -0,0 +1,12 @@
+package chess;
+
+import boardgame.BoardExeption;
+
+public class ChessException extends BoardExeption{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChessException(String msg) {
+		super(msg);
+	}
+}
